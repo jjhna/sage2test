75d4716c0db28f41e879f6968a0388681a455923
Luc Renambot <renambot@uic.edu>
2018-06-07 18:13:56 -0500

    fix: missing app icon when reloading sessions

--- server.js
+++ server.js
@@ -2832,6 +2832,7 @@ function createAppFromDescription(app, callback) {
 		appInstance.previous_width  = app.previous_width;
 		appInstance.previous_height = app.previous_height;
 		appInstance.maximized       = app.maximized;
+		appInstance.icon            = app.icon;
 		sageutils.mergeObjects(app.data, appInstance.data, ['doc_url', 'video_url', 'video_type', 'audio_url', 'audio_type']);
 
 		callback(appInstance, videohandle);
