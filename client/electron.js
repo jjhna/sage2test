c560d6176c3f26e86dc1bd16d7c11804c2548340
Luc Renambot <renambot@uic.edu>
2019-03-11 18:06:49 -0500

    fix: resolve merge issue

--- client/electron.js
+++ client/electron.js
@@ -134,7 +134,6 @@ commander
 	.option('--height <n>',              'Window height (int)', myParseInt, 720)
 	.option('--password <s>',            'Server password (string)', null)
 	.option('--show-fps',                'Display the Chrome FPS counter', false)
-	.option('--local',                   'Used for local development', false)
 	.option('--width <n>',               'Window width (int)', myParseInt, 1280)
 	.parse(args);
 
@@ -581,115 +580,6 @@ function myParseInt(str, defaultValue) {
 function buildMenu() {
 
 	const template = [
-<<<<<<< HEAD
-	{
-		label: 'Edit',
-		submenu: [
-			{
-				label: 'Undo',
-				accelerator: 'CmdOrCtrl+Z',
-				role: 'undo'
-			},
-			{
-				label: 'Redo',
-				accelerator: 'Shift+CmdOrCtrl+Z',
-				role: 'redo'
-			},
-			{
-				type: 'separator'
-			},
-			{
-				label: 'Cut',
-				accelerator: 'CmdOrCtrl+X',
-				role: 'cut'
-			},
-			{
-				label: 'Copy',
-				accelerator: 'CmdOrCtrl+C',
-				role: 'copy'
-			},
-			{
-				label: 'Paste',
-				accelerator: 'CmdOrCtrl+V',
-				role: 'paste'
-			},
-			{
-				label: 'Select All',
-				accelerator: 'CmdOrCtrl+A',
-				role: 'selectall'
-			},
-		]
-	},
-	{
-		label: 'View',
-		submenu: [
-		{
-			label: 'Reload',
-			accelerator: 'CmdOrCtrl+R',
-			click: function(item, focusedWindow) {
-				if (focusedWindow) {
-					focusedWindow.reload();
-				}
-			}
-		},
-		{
-			label: 'Toggle Full Screen',
-			accelerator: (function() {
-				if (process.platform === 'darwin')
-				return 'Ctrl+Command+F';
-				else
-				return 'F11';
-				})(),
-			click: function(item, focusedWindow) {
-				if (focusedWindow) {
-					focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
-				}
-			}
-		},
-		{
-			label: 'Toggle Developer Tools',
-			accelerator: (function() {
-				if (process.platform === 'darwin')
-				return 'Alt+Command+I';
-				else
-				return 'Ctrl+Shift+I';
-				})(),
-			click: function(item, focusedWindow) {
-				if (focusedWindow) {
-					focusedWindow.toggleDevTools();
-				}
-			}
-		}
-		]
-	},
-	{
-		label: 'Window',
-		role: 'window',
-		submenu: [
-		{
-			label: 'Minimize',
-			accelerator: 'CmdOrCtrl+M',
-			role: 'minimize'
-		},
-		{
-			label: 'Close',
-			accelerator: 'CmdOrCtrl+W',
-			role: 'close'
-		}
-		]
-	},
-	{
-		label: 'Help',
-		role: 'help',
-		submenu: [
-		{
-			label: 'Learn More',
-			click: function() {
-				shell.openExternal('http://sage2.sagecommons.org/v4-0-release/sage2-display-client/') }
-			},
-		]
-	}
-=======
 		{
 			label: 'Edit',
 			submenu: [
@@ -800,7 +690,6 @@ function buildMenu() {
 				}
 			]
 		}
->>>>>>> master
 	];
 
 	if (process.platform === 'darwin') {
@@ -808,59 +697,6 @@ function buildMenu() {
 		template.unshift({
 			label: name,
 			submenu: [
-<<<<<<< HEAD
-			{
-				label: 'About ' + name,
-				role: 'about'
-			},
-			{
-				type: 'separator'
-			},
-			{
-				label: 'Services',
-				role: 'services',
-				submenu: []
-			},
-			{
-				type: 'separator'
-			},
-			{
-				label: 'Hide ' + name,
-				accelerator: 'Command+H',
-				role: 'hide'
-			},
-			{
-				label: 'Hide Others',
-				accelerator: 'Command+Shift+H',
-				role: 'hideothers'
-			},
-			{
-				label: 'Show All',
-				role: 'unhide'
-			},
-			{
-				type: 'separator'
-			},
-			{
-				label: 'Quit',
-				accelerator: 'Command+Q',
-				click: function() {
-					app.quit();
-				}
-			},
-		]
-		});
-		const windowMenu = template.find(function(m) { return m.role === 'window' })
-		if (windowMenu) {
-			windowMenu.submenu.push(
-			{
-				type: 'separator'
-			},
-			{
-				label: 'Bring All to Front',
-				role: 'front'
-			});
-=======
 				{
 					label: 'About ' + name,
 					role: 'about'
@@ -914,7 +750,6 @@ function buildMenu() {
 					label: 'Bring All to Front',
 					role: 'front'
 				});
->>>>>>> master
 		}
 	}
 
